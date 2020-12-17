4b2defa41b055babdce4ab4f0248fed17146a001
dmytrochornyy <dima.chornyy@gmail.com>
2020-12-17 23:29:38 +0200

    Changed names of messages variables

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -143,9 +143,9 @@ def letter_checker(letter, letters_guessed, guesses_remaining, warnings_remainin
     """
     # Defining different messages for different cases.
     msg_not_valid = "Oops! That is not a valid letter. You have {} warnings left: {}"
-    msg_nv_no_w_left = "Oops! That is not a valid letter. You have {} warnings left so you lose one guess: {}"
+    msg_not_valid_no_warnings = "Oops! That is not a valid letter. You have {} warnings left so you lose one guess: {}"
     msg_repeated = "Oops! You've already guessed that letter. You now have {} warnings: {}"
-    msg_nv_no_w_left = "Oops! You've already guessed that letter. You have {} warnings left so you lose one guess: {}"
+    msg_repeated_no_warnings = "Oops! You've already guessed that letter. You have {} warnings left so you lose one guess: {}"
     
 
     # If letter is not in alphabet.
@@ -159,7 +159,7 @@ def letter_checker(letter, letters_guessed, guesses_remaining, warnings_remainin
         # If there are not any warnings - subtract guesses.
         else:
             guesses_remaining -= 1
-            return (guesses_remaining, warnings_remaining, False, msg_nv_no_w_left)
+            return (guesses_remaining, warnings_remaining, False, msg_not_valid_no_warnings)
 
     # Checking if letter is already named.
     elif letter in letters_guessed:
@@ -167,7 +167,7 @@ def letter_checker(letter, letters_guessed, guesses_remaining, warnings_remainin
         # If there are no warnings left - subtract guesses
         if warnings_remaining == 0:
             guesses_remaining -= 1
-            return (guesses_remaining, warnings_remaining, False, msg_nv_no_w_left)
+            return (guesses_remaining, warnings_remaining, False, msg_repeated_no_warnings)
 
         # If there are warnings - subtract one
         warnings_remaining -=1
